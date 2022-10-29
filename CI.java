62e38595fc8c859ffd4dbd96d94cd73b7a8d16ac
sameermahto <116938538+sameermahto@users.noreply.github.com>
2022-10-29 11:44:13 +0530

    Create CI.java

---
+++ CI.java
@@ -0,0 +1,17 @@
+
+import java.io.*;
+
+class GFG
+{
+	public static void main(String args[])
+	{
+		double principle = 10000, rate = 10.25, time = 5;
+
+		/* Calculate compound interest */
+		double CI = principle *
+					(Math.pow((1 + rate / 100), time));
+		
+		System.out.println("Compound Interest is "+ CI);
+	}
+}
+
